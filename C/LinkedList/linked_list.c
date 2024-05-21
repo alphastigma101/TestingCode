7027ea15f8cc8f2d84113aca63e174e679452c28
Logan Welty <12logan.welty@gmail.com>
2024-05-21 11:05:04 -0700

    Final push for the linked list

--- C/LinkedList/linked_list.c
+++ C/LinkedList/linked_list.c
@@ -2,7 +2,7 @@
 
 // Memory allocator by Kernighan and Ritchie,
 // The C programming Language, 2nd ed.  Section 8.7.
-
+#define ALLOC 4096
 typedef long Align;
 
 union header {
@@ -18,9 +18,9 @@ union header {
 typedef union header Header;
 
 struct LinkedList {
-  Header head;
-  Header next;
-  Header tail;
+  Header *head;
+  Header *next;
+  Header *tail;
   int set;
 };
 
@@ -71,8 +71,20 @@ static Header* morecore( unsigned int nu) {
 
   return freep;
 }
+// Static memory
+static char memory[ALLOC];
+static size_t memory_index = 0;
+static struct LinkedList *node;
 
-static struct LinkedList node;
+// Allocate memory from the static memory
+void* allocation(size_t size) {
+  if (memory_index + size > ALLOC) {
+        return (void*)0; // Out of memory
+    }
+    void* ptr = &memory[memory_index];
+    memory_index += size;
+    return ptr;
+}
 void* umalloc( unsigned int nbytes) {
   /* 
     - OS must maintain a list of free memory blocks available in the system. 
@@ -81,78 +93,84 @@ void* umalloc( unsigned int nbytes) {
     - If no suitable block is found, the operating system can either wait until a suitable block becomes available 
     - or request additional memory from the system.
   */
-  if (node.set == 0) {
-    node.head.s.size = 512;
-    node.next.s.size = 1028;
-    node.tail.s.size = 2048;
-    node.head.s.ptr = &node.next;
-    node.next.s.ptr = &node.tail;
-    node.set = 1;
+  if (node == (void*)0) {
+    node = (struct LinkedList *)allocation(sizeof(struct LinkedList));
+    node->head = (Header*)allocation(sizeof(Header));
+    node->head->s.size = 512;
+    node->next = (Header*)allocation(sizeof(Header));
+    node->next->s.size = 1028;
+    node->tail = (Header*)allocation(sizeof(Header));
+    node->tail->s.size = 2048;
+    node->head->s.ptr = node->next;
+    node->next->s.ptr = node->tail;
+    node->tail->s.ptr = (void*)0;
   }
   unsigned int nunits;
   nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
-  for(; ( node.head.s.ptr) != (void*)0; node.head = *node.head.s.ptr ) {
+  for(; ( node->head->s.ptr) != (void*)0; node->head = node->head->s.ptr ) {
     /*
       Layout:
         - [head|size] -> [next|size] -> [tail|size]
       Notes:
         - Each node has a pointer that type that points to it's own node respectively 
     */
-    if( node.head.s.size < node.next.s.size && node.head.s.size >= nunits ) {
+    if( node->head->s.size > node->next->s.size && node->head->s.size >= nunits ) {
       printf("=======================================================\n");
       printf("Found a block that can accommodate %u units\n", nunits);
       printf("=======================================================\n");
-      node.head.s.size -= nunits;
-      freep = &node.head; // head is the current best fit 
+      node->head->s.size -= nunits;
+      freep = node->head; // head is the current best fit 
     }
-    if(node.next.s.size < node.tail.s.size && node.next.s.size >= nunits) {
-      if (node.head.s.size < node.next.s.size < node.tail.s.size) {
+    if(node->next->s.size > node->tail->s.size && node->next->s.size >= nunits) {
         printf("=======================================================\n");
         printf("Found the best fit for the accommodated block %u units\n", nunits);
         printf("=======================================================\n");
         printf("=======================================================\n");
-        printf("Head block size: %d\n", node.head.s.size);
-        printf("Next block size: %d\n", node.next.s.size);
-        printf("Tail block size: %d\n", node.tail.s.size);
+        printf("Head block size: %d\n", node->head->s.size);
+        printf("Next block size: %d\n", node->next->s.size);
+        printf("Tail block size: %d\n", node->tail->s.size);
         printf("=======================================================\n");
-        printf("Head block pointer: %p\n", node.head.s.ptr);
-        printf("Next block pointer: %p\n", node.next.s.ptr); 
-        printf("Tail block pointer: %p\n", node.tail.s.ptr);  
-        return (void*)(&node.head + 1);
-      }
-      else {
-        printf("=======================================================\n");
-        printf("Splitting all blocks\n");
-        printf("=======================================================\n");
-        // Reduce it again 
-        node.next.s.size -= nunits;
-        node.tail.s.size -= nunits;
-        freep = &node.next;
-        /* Debugging: */
-        printf("=======================================================\n");
-        printf("Head block size: %d\n", node.head.s.size);
-        printf("Next block size: %d\n", node.next.s.size);
-        printf("Tail block size: %d\n", node.tail.s.size);
-        printf("=======================================================\n");
-        printf("Head block pointer: %p\n", node.head.s.ptr);
-        printf("Next block pointer: %p\n", node.next.s.ptr); // Note: If you see next pointing to null that is good because that means the linked list has not grown yet
-        printf("Tail block pointer: %p\n", node.tail.s.ptr); // Note: If you see tail pointing to null that is good because that means the linked list has not grown yet  
-      }
+        printf("Head block pointer: %p\n", node->head->s.ptr);
+        printf("Next block pointer: %p\n", node->next->s.ptr); 
+        printf("Tail block pointer: %p\n", node->tail->s.ptr);
+        node->head->s.size -= nunits;
+        freep = node->next;
+        return (void*)(node->next + 1);
     }
-    if ( node.head.s.size == nunits && node.next.s.size == nunits && node.tail.s.size == nunits ) {
+    else {
+      printf("=======================================================\n");
+      printf("Splitting blocks\n");
+      printf("=======================================================\n");
+      node->tail->s.size -= nunits;
+      freep = node->tail;
+      /* Debugging: */
+      printf("=======================================================\n");
+      printf("Head block size: %d\n", node->head->s.size);
+      printf("Next block size: %d\n", node->next->s.size);
+      printf("Tail block size: %d\n", node->tail->s.size);
+      printf("=======================================================\n");
+      printf("Head block pointer: %p\n", node->head->s.ptr);
+      printf("Next block pointer: %p\n", node->next->s.ptr); // Note: If you see next pointing to null that is good because that means the linked list has not grown yet
+      printf("Tail block pointer: %p\n", node->tail->s.ptr); // Note: If you see tail pointing to null that is good because that means the linked list has not grown yet
+      return (void*)(node->tail + 1);
+    }
+    if ( node->head->s.size == nunits && node->next->s.size == nunits && node->tail->s.size == nunits ) {
       printf("=======================================================\n");
       printf("Exact fit found, splitting the block\n");
       printf("=======================================================\n");
-      node.head.s.size -= nunits;
-      node.next.s.size -= nunits;
-      node.tail.s.size -= nunits;
-      freep = &node.head;
+      node->head->s.size -= nunits;
+      node->next->s.size -= nunits;
+      node->tail->s.size -= nunits;
+      freep = node->head;
+      return (void*)(node->head + 1);
     }
     else {
       // Start adding sub nodes to the parent node (next)
       if (freep == (void*)0) {
         /* Start adding sub nodes to the parent node (next) if:
            1. freep was not set with a node from the linked list. This means that best fit was not found
+           2. You want to make things harder
+           3. Otherwise just keep adding nodes at the tail end
         */                                       
         /* New Layout:                           ^
                                                  |
@@ -161,15 +179,17 @@ void* umalloc( unsigned int nbytes) {
                                       |_______________________|
                                                   ^ 
                                                   |               
-           _______________________       _______________________        _______________________
-          |       size, ptr       | --->|       size, ptr       |----> |    size,ptr           |
-          |_______________________|     |_______________________|      |_______________________|
-        */
+           _______________________       _______________________        _______________________       __________________
+          |       size, ptr       | --->|       size, ptr       |----> |    size,ptr           | --> | size,ptr         |
+          |_______________________|     |_______________________|      |_______________________|     |__________________|
+                                                                                                     
+        */                                             
         printf("=======================================================\n");
         printf("No suitable block found, making one\n");
         printf("=======================================================\n");
-        node.next.s.ptr = morecore(nunits);
-        freep = &node.next; // You need to set freep to the parent node so it doesn't crash the function free
+        freep = node->tail;
+        node->tail->s.ptr = morecore(nunits);
+        return (void*)(node->tail + 1);
       }
     }
   }
