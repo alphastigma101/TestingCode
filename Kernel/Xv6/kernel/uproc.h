78260799a60184676bc08dae384a343994fe3aba
Logan <12logan.welty@gmail.com>
2024-06-18 14:14:38 -0700

    This is the ArchLinux Branch where I will include all the custom applications that fit my needs

--- Kernel/Xv6/kernel/uproc.h
+++
@@ -1,9 +0,0 @@
-
-struct uproc {
-   int pid;
-   char name[16];
-   char state[7];
-   int ticks;
-   int avialable_tokens;
-   int pticks;
-};
--- Kernel/Xv6/user/shrimpy.c
+++
@@ -1,8 +0,0 @@
-#include "user/user.h"
-shrimpy_t *Shrimpy;
-int main(void) {
-  printf("========================\n");
-  printf("Running tests for shrimpy...\n");
-  shrimpy(Shrimpy);
-  return 0;
-}
